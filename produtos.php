789a622f5b4330409a399dbc31c4864117221894
Paulo <paulohenriquepatarello@gmail.com>
2020-06-22 14:18:51 -0300

    produtos em xml

--- produtos.php
+++ produtos.php
@@ -1,30 +1,38 @@
-      <div class="prod_box">
-        <div class="top_prod_box"></div>
-        <div class="center_prod_box">
-          <div class="product_title"><a href="detalhes.php">Motorola 156 MX-VL</a></div>
-          <div class="product_img"><a href="detalhes.php"><img src="images/laptop.gif" alt="" border="0" /></a></div>
-          <div class="prod_price"><span class="reduce">R$ 350</span> <span class="price">R$ 270</span></div>
+
+
+<?php 
+  $link= "produtos.xml"; 
+  $xml = simplexml_load_file($link) -> produtos;
+
+  foreach($xml-> produto as $produto){ 
+   echo '<div class="prod_box">
+              <div class="top_prod_box"></div>
+                <div class="center_prod_box">
+                  <div class="product_title">
+                    <a href="detalhes.php">'.$produto->nome.'</a>
+                  </div>
+            <div class="product_img">
+              <a href="detalhes.php">
+                <img src="images/'.$produto->imagem.'" alt="" border="0" height="150px" width="150px"/>
+                </a>
+            </div>
+
+          <div class="prod_price">
+            <span class="price">'.$produto->preco.'</span>
+          </div>
+
         </div>
         <div class="bottom_prod_box"></div>
         <div class="prod_details_tab"> <a href="#" title="header=[Add to cart] body=[&nbsp;] fade=[on]"><img src="images/cart.gif" alt="" border="0" class="left_bt" /></a> <a href="#" title="header=[Specials] body=[&nbsp;] fade=[on]"><img src="images/favs.gif" alt="" border="0" class="left_bt" /></a> <a href="#" title="header=[Gifts] body=[&nbsp;] fade=[on]"><img src="images/favorites.gif" alt="" border="0" class="left_bt" /></a> <a href="detalhes.php" class="prod_details">detalhes</a> </div>
-      </div>
-      <div class="prod_box">
-        <div class="top_prod_box"></div>
-        <div class="center_prod_box">
-          <div class="product_title"><a href="detalhes.php">Iphone Apple</a></div>
-          <div class="product_img"><a href="detalhes.php"><img src="images/p4.gif" alt="" border="0" /></a></div>
-          <div class="prod_price"><span class="price">R$ 270</span></div>
-        </div>
-        <div class="bottom_prod_box"></div>
-        <div class="prod_details_tab"> <a href="#" title="header=[Add to cart] body=[&nbsp;] fade=[on]"><img src="images/cart.gif" alt="" border="0" class="left_bt" /></a> <a href="#" title="header=[Specials] body=[&nbsp;] fade=[on]"><img src="images/favs.gif" alt="" border="0" class="left_bt" /></a> <a href="#" title="header=[Gifts] body=[&nbsp;] fade=[on]"><img src="images/favorites.gif" alt="" border="0" class="left_bt" /></a> <a href="detalhes.php" class="prod_details">detalhes</a> </div>
-      </div>
-      <div class="prod_box">
-        <div class="top_prod_box"></div>
-        <div class="center_prod_box">
-          <div class="product_title"><a href="detalhes.php">Samsung Webcam</a></div>
-          <div class="product_img"><a href="detalhes.php"><img src="images/p5.gif" alt="" border="0" /></a></div>
-          <div class="prod_price"><span class="reduce">R$ 350</span> <span class="price">R$ 270</span></div>
-        </div>
-        <div class="bottom_prod_box"></div>
-        <div class="prod_details_tab"> <a href="#" title="header=[Add to cart] body=[&nbsp;] fade=[on]"><img src="images/cart.gif" alt="" border="0" class="left_bt" /></a> <a href="#" title="header=[Specials] body=[&nbsp;] fade=[on]"><img src="images/favs.gif" alt="" border="0" class="left_bt" /></a> <a href="#" title="header=[Gifts] body=[&nbsp;] fade=[on]"><img src="images/favorites.gif" alt="" border="0" class="left_bt" /></a> <a href="detalhes.php" class="prod_details">detalhes</a> </div>
-      </div>
+      </div>';
+  }
+      
+
+    ?>
+
+
+
+
+
+
+      
